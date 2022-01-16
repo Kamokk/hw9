b8e2f51a15c80d21561f7019cb6f6eedd08e5b9f
C1 <yarievakamila@gmail.com>
2022-01-16 20:32:45 +0300

    delete string breakpoint, delete not used import, reformat code

--- src/test/java/TestDropdownList.java
+++ src/test/java/TestDropdownList.java
@@ -1,21 +1,20 @@
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class TestDropdownList {
     @Test
-     void testDropdownList() {
-        open ("https://the-internet.herokuapp.com/dropdown");
+    void testDropdownList() {
+        open("https://the-internet.herokuapp.com/dropdown");
         $("#dropdown").click();
         $("#dropdown").$(byText("Option 1")).click();
-$("option[selected=selected]").shouldHave(text("Option 1"));
+        $("option[selected=selected]").shouldHave(text("Option 1"));
 
-        System.out.println("qwqwqw");
+
     }
-
 
 
 }
